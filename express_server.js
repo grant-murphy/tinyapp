1bfa02ff38bd3a96f62f212ad27a1c3fbb612406
grant-murphy <murphyg.gm@gmail.com>
2019-07-09 20:27:12 +0000

    Added Delete and Update feature

--- express_server.js
+++ express_server.js
@@ -55,6 +55,16 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect(`/urls/`);
 });
 
+app.post("/urls/:shortURL/edit", (req, res) => {
+  res.redirect(`/urls/${req.params.shortURL}`);
+});
+
+app.post("/urls/:shortURL", (req, res) => {
+  console.log(req.params)
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect(`/urls/`);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
